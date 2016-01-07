9631d949c672aad28d606211fd8747fe0a3839e4
matt kocaj <mkocaj@hotmail.com>
2016-01-07 15:37:39 +0800

    added test project with simple home page

---
+++ DevCookie.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DevCookie.Web.Startup))]
+
+namespace DevCookie.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
