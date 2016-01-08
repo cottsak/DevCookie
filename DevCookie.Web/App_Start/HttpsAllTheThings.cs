63608dabd3ec58c71f420fc95820e93bf529af16
matt kocaj <mkocaj@hotmail.com>
2016-01-08 09:30:17 +0800

    cleaning up

---
+++ DevCookie.Web/App_Start/HttpsAllTheThings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace DevCookie.Web
+{
+    /// <summary>
+    /// Redirect all non-HTTPS traffic to HTTPS (for MVC controllers)
+    /// </summary>
+    public class HttpsAllTheThings : ActionFilterAttribute
+    {
+        private readonly int? _sslPort;
+
+        public HttpsAllTheThings(int? sslPort = null)
+        {
+            _sslPort = sslPort;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // if request is HTTPS then OK
+            if (filterContext.HttpContext.Request.IsSecureConnection)
+            {
+                return;
+            }
+
+            // if load banalcer is doing 'ssl offloading' it will add a header indicating that 
+            // the request is SSL secured even tho it's not HTTPS by the time it gets here.
+            if (string.Equals(filterContext.HttpContext.Request.Headers["X-Forwarded-Proto"], "https",
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                return;
+            }
+
+            // if not HTTPS then redirect to HTTPS
+            var request = filterContext.RequestContext.HttpContext.Request.Url;
+            var redirectToHttps = $"https://{request.Host}{(_sslPort.HasValue ? ":" + _sslPort.Value : string.Empty)}{request.PathAndQuery}";
+            filterContext.Result = new RedirectResult(redirectToHttps);
+        }
+    }
+}
--- DevCookie.Web/Controllers/HomeController.cs
+++ DevCookie.Web/Controllers/HomeController.cs
@@ -1,48 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using RequireHttpsAttributeBase = System.Web.Mvc.RequireHttpsAttribute;
+﻿using System.Web.Mvc;
 
 namespace DevCookie.Web.Controllers
 {
     public class HomeController : Controller
     {
-        //[RequireHttps]
         public ActionResult Index()
         {
             return View();
-        }
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true,
-        AllowMultiple = false)]
-    public class RequireHttpsAttribute : RequireHttpsAttributeBase
-    {
-        public override void OnAuthorization(AuthorizationContext filterContext)
-        {
-            if (filterContext == null)
-            {
-                throw new ArgumentNullException("filterContext");
-            }
-
-            if (filterContext.HttpContext.Request.IsSecureConnection)
-            {
-                return;
-            }
-
-            if (string.Equals(filterContext.HttpContext.Request.Headers["X-Forwarded-Proto"], "https", StringComparison.InvariantCultureIgnoreCase))
-            {
-                return;
-            }
-
-            if (filterContext.HttpContext.Request.IsLocal)
-            {
-                return;
-            }
-
-            HandleNonHttpsRequest(filterContext);
         }
     }
 }
