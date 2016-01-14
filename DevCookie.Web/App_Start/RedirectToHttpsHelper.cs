33650e412957d016ef28e8390682c3327336f683
matt kocaj <mkocaj@hotmail.com>
2016-01-15 06:58:09 +0800

    yay! ssl is working https://appharbor.com/applications/devcookie/builds/1661132 cleaning up.

---
+++ DevCookie.Web/App_Start/RedirectToHttpsHelper.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace DevCookie.Web
+{
+    internal static class RedirectToHttpsHelper
+    {
+        internal static void RedirectToHttps(this IAppBuilder appBuilder, string sslPortAppSettingKey)
+        {
+            RedirectToHttps(appBuilder, ConfigurationManager.AppSettings[sslPortAppSettingKey] == null ? (int?)null
+                : Convert.ToInt32(ConfigurationManager.AppSettings[sslPortAppSettingKey]));
+        }
+
+        internal static void RedirectToHttps(this IAppBuilder appBuilder, int? sslPort = null)
+        {
+            appBuilder.Use((context, next) =>
+            {
+                if (IsSecure(context.Request))
+                    return next.Invoke();
+
+                // connection is not secure, so redirect to HTTPS
+                context.Response.Redirect(string.Format("https://{0}{1}{2}",
+                    context.Request.Uri.Host,
+                    sslPort.HasValue ? ":" + sslPort.Value : string.Empty,
+                    context.Request.Uri.PathAndQuery));
+
+                // end the pipeline so nothing is processed insecurely
+                return Task.FromResult(0);
+            });
+        }
+
+        private static bool IsSecure(IOwinRequest request)
+        {
+            return request.Uri.Scheme == "https"
+                || string.Equals(request.Headers["X-Forwarded-Proto"], "https", StringComparison.InvariantCultureIgnoreCase);  // from load balancer
+        }
+    }
+}
--- DevCookie.Web/Startup.cs
+++ DevCookie.Web/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Configuration;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 using DevCookie.Web;
 using Microsoft.Owin;
@@ -18,51 +14,9 @@ namespace DevCookie.Web
         {
             app.RedirectToHttps("ssl-port");
 
-            //#if DEBUG
-            //            GlobalFilters.Filters.Add(new HttpsAllTheThings(44300), -100);  // IIS Express; -100 means before all other global filters
-            //            // todo: same for webapi
-            //#else
-            //            GlobalFilters.Filters.Add(new HttpsAllTheThings(), -100);
-            //#endif
-
             GlobalFilters.Filters.Add(new DevCookieAuthorizeAttribute());
 
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
-    }
-
-    public static class RedirectToHttpsExtensions
-    {
-        public static void RedirectToHttps(this IAppBuilder appBuilder, string sslPortAppSettingKey)
-        {
-            RedirectToHttps(appBuilder,
-                ConfigurationManager.AppSettings[sslPortAppSettingKey] == null ? (int?)null
-                : Convert.ToInt32(ConfigurationManager.AppSettings[sslPortAppSettingKey])
-                );
-        }
-
-        public static void RedirectToHttps(this IAppBuilder appBuilder, int? sslPort = null)
-        {
-            appBuilder.Use((context, next) =>
-            {
-                if (IsSecure(context.Request))
-                    return next.Invoke();
-
-                // connection is not secure, so redirect to HTTPS
-                context.Response.Redirect(string.Format("https://{0}{1}{2}",
-                    context.Request.Uri.Host,
-                    sslPort.HasValue ? ":" + sslPort.Value : string.Empty,
-                    context.Request.Uri.PathAndQuery));
-
-                // end the pipeline so nothing is processed insecurely
-                return Task.FromResult(0);
-            });
-        }
-
-        private static bool IsSecure(IOwinRequest request)
-        {
-            return request.Uri.Scheme == "https"
-                || string.Equals(request.Headers["X-Forwarded-Proto"], "https", StringComparison.InvariantCultureIgnoreCase);  // from load balancer
         }
     }
 }
