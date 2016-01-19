793fdd0af36df5c23b633671878565952c217028
matt kocaj <mkocaj@hotmail.com>
2016-01-19 17:04:59 +0800

    adding other pages to test with

---
+++ DevCookie.Web/Controllers/OtherController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevCookie.Web.Controllers
+{
+    public class OtherController : Controller
+    {
+        // GET: Other
+        public ActionResult Index()
+        {
+            return RedirectToAction("Page");
+        }
+
+        public ActionResult Page()
+        {
+            return View();
+        }
+    }
+}
