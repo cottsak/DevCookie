567e3faffcd67c3cb3475976a048c826a275b04c
matt kocaj <mkocaj@hotmail.com>
2016-01-20 16:24:45 +0800

    updating metadata for nuget packaging

--- DevCookie/Properties/AssemblyInfo.cs
+++ DevCookie/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DevCookie")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A helper library indended to enable rapid go-to-prod by providing early shared-key cookie based auth and Feature Toggling")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("https://twitter.com/mattkocaj")]
 [assembly: AssemblyProduct("DevCookie")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("CC BY 4.0 https://creativecommons.org/licenses/by/4.0/")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
